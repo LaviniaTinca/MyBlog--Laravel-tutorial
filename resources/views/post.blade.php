4935bc7e7668caafc4220b829169d2fed355bcd2
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-07-05 13:56:37 +0300

    Blade 1

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Post</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <article>
-        <h1><?=$post->title;?></h1>
+@extends('layout')
+
+@section('content')
+<article>
+        <h1>{{$post->title}}</h1>
         <div>
-            <?= $post->body;?>
+             {!!$post->body!!} <!-- we want this as html-->
         </div>
     </article>
     <a href="/">Go Back</a>
-</body>
-</html>
+@endsection
