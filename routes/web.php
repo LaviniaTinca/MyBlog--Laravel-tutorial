432665f955eeba68e5fd01aaac32707633b02d6f
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-07-09 20:10:30 +0300

    register user page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -171,3 +172,6 @@ Route::get('authors/{author:username}', function (User $author) {
         'categories' => Category::all()
     ]);
 });
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
