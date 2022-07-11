6924158af1eb06a06531840d46abb5a6f9b9acff
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-07-11 20:05:03 +0300

    Comments- design a form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -53,6 +53,10 @@ class Post extends Model
 
 
     //eloquent relationship
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
     public function category()
     {
         //hasOne, hasMany, belongsTo, belongsToMany
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -47,6 +47,29 @@
                         {!!$post->body!!}
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                    <form method="POST" action="#" >
+                    @csrf
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/100?u={{ Auth::id() }}" alt="" width="40" height="60" class="rounded-full">
+                            <h2 class="ml-4">Want to post a comment?</h2>
+                        </header>
+                        <div class="mt-6">
+                            <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="5" placeholder="Your comment here!"></textarea>
+                        </div>
+                        <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                            <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                        </div>
+                    </form>
+                    </x-panel>
+                    
+
+                    @foreach ($post->comments as $comment)
+                        <x-post-comment :comment="$comment" />
+                    @endforeach
+                </section>
             </article>
         </main>
 
